316836485a1bf7c8a1b7c2a2bbac8026477d598d
aavdeev01 <aavdeev01@icloud.com>
2020-12-17 19:59:19 +0300

    init commit

---
+++ src/main/java/com/example/test/demo/TestClickerApplication.java
@@ -0,0 +1,13 @@
+package com.example.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestClickerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestClickerApplication.class, args);
+    }
+
+}
